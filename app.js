107d6e8a23ef88b380997227d3c39f9196875b02
yurika0501 <titizuzu0501@gmail.com>
2023-04-01 10:55:53 +0000

    [Add]jQuery練習

--- app.js
+++ app.js
@@ -10,12 +10,17 @@
 // });
 
 
+// $(function(){
+//   $('.box1').mouseover(function(){
+//       $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//       $('.box1').removeClass('box1-ext');
+//   });
+// });
+
 $(function(){
-  $('.box1').slideDown(function(){
-      $('.box1').css({
-          'background-color': '#0000FF',
-          'width':'200px',
-          'height':'100px'
-      }).slideUp();
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
